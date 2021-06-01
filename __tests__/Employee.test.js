dc89d5d905a722364fc0c328783bc09b59daeb64
Leigh Dahlin <leigh.dahlin@gmail.com>
2021-06-01 12:52:51 -0700

    used async to get prompts to run one after the other

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,1 +1,52 @@
-const Employee = require('./employee');
+
+const { expect } = require('@jest/globals');
+const Employee = require('../lib/employee');
+
+describe('Initilization', () => {
+    it("Name", () => {
+        const name = "John";
+
+        const newEmployee = new Employee(name,1,"john@test.com");
+        const result = newEmployee.name;
+
+        expect(result).toEqual(result);
+    });
+    it("Id", () => {
+        const id = 1;
+
+        const newEmployee = new Employee("John",id,"john@test.com");
+        const result = newEmployee.id;
+
+        expect(result).toEqual(result);
+    });
+    it("Email", () => {
+        const email = "john@test.com";
+
+        const newEmployee = new Employee("John",1,email);
+        const result = newEmployee.email;
+
+        expect(result).toEqual(result);
+    });
+});
+
+describe("Data validity, are the values expected?", () => {
+    it("Should throw an error if name provided is not string value", ()=> {
+        const invalidName = 2;
+        const error = new Error ("Expected parameter name to be non-empty string");
+
+        const cb = () => new Employee(invalidName,"1","john@test.com")
+
+        expect(cb).toThrow(error)
+    });
+    // it("Should throw an error if id provided is not a number", ()=> {
+    //     const invalidId = "John";
+    //     const error = new Error ("Expected parameter id to be non-empty number");
+
+    //     const cb = () => new Employee("John", invalidId, "john@test.com")
+
+    //     expect(cb).toThrow(error);
+    // })
+}) 
+
+//additional tests: test email validation
+//test formulas
--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const inquirer = require("inquirer");
+const fs = require("fs");
+const employee = require("../lib/employee");
+const manager = require("../lib/manager");
+const engineer = require("../lib/engineer");
+const intern = require("../lib/intern")
 
 inquirer
+
+const manager = new Manager()
+manager.getName()
+manager.getId()
