7eb0cfd5ca5ddddf5ea531c4a56621e7c8bb810e
Leigh Dahlin <leigh.dahlin@gmail.com>
2021-06-02 08:02:48 -0700

    imported subclasses into index.js file and create prompt to add intern or engineer and corresponding object is created and push to teamMembers array, array will be used to generate the html file

--- index.js
+++ index.js
@@ -1,12 +1,67 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const employee = require("../lib/employee");
-const manager = require("../lib/manager");
-const engineer = require("../lib/engineer");
-const intern = require("../lib/intern")
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern")
+const teamMembers = [];
 
-inquirer
+const generatorQs = ()=> {
+    return inquirer.prompt([
+        {
+            type: 'list',
+            name: 'create',
+            message: 'Please select an option below',
+            choices: [
+                'Add an engineer',
+                'Add an intern',
+                'Finish',
+            ]
+        },
+    ])
+    .then((answer) => {
+        if(answer.create === "Add an engineer") {
+            createEngineer();
+        } else if (answer.create === "Add an intern") {
+            createIntern();
+        } else {
+            console.log(teamMembers)
+            //name the file a combiation of the office number and manager's name
+            const fileName = teamMembers[0].officeNumber + "-" + teamMembers[0].name
+            console.log(fileName)
+        }
 
-const manager = new Manager()
-manager.getName()
-manager.getId()
+    })
+}
+
+//function to use the Manager subclass to create an object for the manager and push it into the teamMember's array
+async function createManager() {
+    const newManager = await new Manager();
+    await newManager.getOfficeNumber();
+    // await console.log(newManager);
+    await teamMembers.push(newManager);
+    // await console.log(teamMembers)
+    await generatorQs();
+}
+
+//function to use the Engineer subclass to create an object for the manager and push it into the teamMember's array
+async function createEngineer() {
+    const newEngineer = await new Engineer();
+    await newEngineer.getGithub();
+    // await console.log(newEngineer);
+    await teamMembers.push(newEngineer);
+    // await console.log(teamMembers)
+    await generatorQs();
+}
+
+//function to use the Intern subclass to create an object for the manager and push it into the teamMember's array
+async function createIntern() {
+    const newIntern = await new Intern();
+    await newIntern.getSchool();
+    // await console.log(newIntern);
+    await teamMembers.push(newIntern);
+    // await console.log(teamMembers)
+    await generatorQs();
+}
+
+//starts the profile generator by first getting manager information and then running the choices of adding or finishing
+createManager();
