23ef6bdd705c728493b1183cc83dd00456153bf1
Leigh Dahlin <leigh.dahlin@gmail.com>
2021-06-02 15:22:33 -0700

    added validate for type of input, string or number

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,43 +1,44 @@
 
-const { expect } = require('@jest/globals');
+const inquirer = require('inquirer');
 const Employee = require('../lib/employee');
 
+jest.mock('inquirer')
+
 describe('Initilization', () => {
-    it("Name", () => {
-        const name = "John";
+    it("user input", async () => {
+        // expect.assertions(1);
+        inquirer.prompt = jest.fn().mockResolvedValue({name: 'Bob'})
 
-        const newEmployee = new Employee(name,1,"john@test.com");
-        const result = newEmployee.name;
+        await expect(Employee).resolves.toEqual({name:'Bob'})
 
-        expect(result).toEqual(result);
     });
-    it("Id", () => {
-        const id = 1;
+    // it("Id", () => {
+    //     const id = 1;
 
-        const newEmployee = new Employee("John",id,"john@test.com");
-        const result = newEmployee.id;
+    //     const newEmployee = new Employee("John",id,"john@test.com");
+    //     const result = newEmployee.id;
 
-        expect(result).toEqual(result);
-    });
-    it("Email", () => {
-        const email = "john@test.com";
+    //     expect(result).toEqual(result);
+    // });
+    // it("Email", () => {
+    //     const email = "john@test.com";
 
-        const newEmployee = new Employee("John",1,email);
-        const result = newEmployee.email;
+    //     const newEmployee = new Employee("John",1,email);
+    //     const result = newEmployee.email;
 
-        expect(result).toEqual(result);
-    });
+    //     expect(result).toEqual(result);
+    // });
 });
 
-describe("Data validity, are the values expected?", () => {
-    it("Should throw an error if name provided is not string value", ()=> {
-        const invalidName = 2;
-        const error = new Error ("Expected parameter name to be non-empty string");
+// describe("Data validity, are the values expected?", () => {
+//     it("Should throw an error if name provided is not string value", ()=> {
+//         const invalidName = 2;
+//         const error = new Error ("Expected parameter name to be non-empty string");
 
-        const cb = () => new Employee(invalidName,"1","john@test.com")
+//         const cb = () => new Employee(invalidName,"1","john@test.com")
 
-        expect(cb).toThrow(error)
-    });
+//         expect(cb).toThrow(error)
+//     });
     // it("Should throw an error if id provided is not a number", ()=> {
     //     const invalidId = "John";
     //     const error = new Error ("Expected parameter id to be non-empty number");
@@ -46,7 +47,7 @@ describe("Data validity, are the values expected?", () => {
 
     //     expect(cb).toThrow(error);
     // })
-}) 
+// }) 
 
 //additional tests: test email validation
 //test formulas
